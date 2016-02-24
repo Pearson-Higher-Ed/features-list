d2bc4ee6db99869996e074403a50609d40b391f3
S Rizmy <VRIZMSA@LKSRIWL-CNU2429.PEROOT.com>
2016-02-24 11:23:17 +0530

    - marketing feature component read onlt mode

--- src/html/templateCell.js
+++ src/html/templateCell.js
@@ -1,7 +1,6 @@
 
 
-var templateCell = 
-
+var templateCell =
 		'<div class="o-feature-brand">{{primaryTitle}}</div>' +
 		'<div class="o-feature-content"> ' +
 			'<div class="o-feature-left"> ' +
@@ -10,17 +9,17 @@ var templateCell =
 				'</header> ' +
 				'<div class="o-feature-description"> ' +
 					'<p>{{description}}</p> ' +
-				'</div> ' +
+				'</div> ' +				
+		'<div class="o-feature-button"> ' +
+			'<button class=\"o-feature-action-button\" href="{{ctaUrl}}"><div>{{ctaText}}<\/div></button> ' +
+		'</div> '
 			'</div> ' +
 			'<div class="o-feature-right"> ' +
 				'<div class="o-feature-img-border"> ' +
 					'<img src="{{resourceUrl}}"> ' +
 				'</div> ' +
 			'</div> ' +
-		'</div> ' +
-		'<div class="o-feature-clearfix"></div> ' +
-		'<div class="o-feature-button"> ' +
-			'<button class=\"o-feature-action-button\" href="{{ctaUrl}}"><div>{{ctaText}}<\/div></button> ' +
+			'<div class="o-feature-clearfix"></div>'+
 		'</div> ';
 
 module.exports = templateCell;
