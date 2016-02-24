8da87eb6d2513b2640586737fd5570e4cfd34577
S Rizmy <VRIZMSA@LKSRIWL-CNU2429.PEROOT.com>
2016-02-24 11:37:55 +0530

    Template Cell Updated

--- src/html/templateCell.js
+++ src/html/templateCell.js
@@ -1,25 +1,5 @@
 
 
-var templateCell =
-		'<div class="o-feature-brand">{{primaryTitle}}</div>' +
-		'<div class="o-feature-content"> ' +
-			'<div class="o-feature-left"> ' +
-				'<header class="o-feature-title"> ' +
-					'{{secondaryTitle}} ' +
-				'</header> ' +
-				'<div class="o-feature-description"> ' +
-					'<p>{{description}}</p> ' +
-				'</div> ' +				
-		'<div class="o-feature-button"> ' +
-			'<button class=\"o-feature-action-button\" href="{{ctaUrl}}"><div>{{ctaText}}<\/div></button> ' +
-		'</div> '
-			'</div> ' +
-			'<div class="o-feature-right"> ' +
-				'<div class="o-feature-img-border"> ' +
-					'<img src="{{resourceUrl}}"> ' +
-				'</div> ' +
-			'</div> ' +
-			'<div class="o-feature-clearfix"></div>'+
-		'</div> ';
-
+var templateCell = '<div class="o-feature-brand">{{primaryTitle}}</div>' +'<div class="o-feature-content"> ' +'<div class="o-feature-left"> ' +'<header class="o-feature-title"> ' +'{{secondaryTitle}} ' + '</header> ' +'<div class="o-feature-description"> ' + '<p>{{description}}</p> ' + '</div>'+'<div class="o-feature-button"> ' + '<button class=\"o-feature-action-button\" href="{{ctaUrl}}"><div>{{ctaText}}</div></button> ' + '</div></div> '+'<div class="o-feature-right">'+'<div class="o-feature-img-border">' + '<img src="{{resourceUrl}}">' + '</div>' + '</div>'
+   + '</div>'+'<div class="o-feature-clearfix"></div>';
 module.exports = templateCell;
