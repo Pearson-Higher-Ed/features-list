9bb16482ca278ec01877ec0b8be4010cb23cad56
S Rizmy <vrizmsa@LKSRIWL-CNU2429.PEROOT.com>
2016-03-18 15:40:56 +0530

    Image and content order changed to support responsive

--- src/html/templateCell.js
+++ src/html/templateCell.js
@@ -1,6 +1,8 @@
-
-
-var templateCell = '<div class="o-feature-content"> '
+var templateCell ='<div class="o-feature-content"> '
+	+ '<div class="o-feature-right">'
+    + '{{#hasImage}}<div class="o-feature-img-border">'
+    + '<img width="160" height="160" src="{{resourceUrl}}">'
+    + '</div>{{/hasImage}}' + '</div>'
     + '<div class="o-feature-left">'
     + '<div class="o-feature-brand">{{primaryTitle}}</div>'
     + '<header class="o-feature-title"> '
@@ -9,9 +11,8 @@ var templateCell = '<div class="o-feature-content"> '
     + '<p>{{description}}</p> ' + '</div>'
     + '{{#hasCTA}}<div class="o-feature-button clearfix"> '
     + '<a class=\"o-feature-action-button\" href="{{ctaUrl}}" target="_blank"><div>{{ctaText}}<\/div></a> '
-    + '</div>{{/hasCTA}}</div> ' + '<div class="o-feature-right">'
-    + '{{#hasImage}}<div class="o-feature-img-border">'
-    + '<img width="160" height="160" src="{{resourceUrl}}">'
-    + '</div>{{/hasImage}}' + '</div>' + '</div>'
+    + '</div>{{/hasCTA}}</div> ' + '</div>'
     + '<div class="o-feature-clearfix"></div>';
 module.exports = templateCell;
+
+
