a3ce61d65d63fc3697bd185b3a036217e5e4abaf
Sanjaya M <sanjaya.marambage@pearson.com>
2016-03-24 15:51:19 +0530

    fix mobile view issues

--- src/html/templateCell.js
+++ src/html/templateCell.js
@@ -1,18 +1,16 @@
-var templateCell ='<div class="o-feature-content"> '
-	+ '<div class="o-feature-right">'
-    + '{{#hasImage}}<div class="o-feature-img-border">'
-    + '<img width="160" height="160" src="{{resourceUrl}}">'
-    + '</div>{{/hasImage}}' + '</div>'
+var templateCell = '<div class="o-feature-content"> '
     + '<div class="o-feature-left">'
     + '<div class="o-feature-brand">{{primaryTitle}}</div>'
     + '<header class="o-feature-title"> '
     + '{{secondaryTitle}} ' + '</header> '
     + '<div class="o-feature-description"> '
-    + '<p>{{description}}</p> ' + '</div>'
+    + '<p>' + '{{#hasImage}}'
+    + '<img class="o-feature-img pull-right" width="160" height="160" src="{{resourceUrl}}">'
+    + '{{/hasImage}}'
+    + '{{description}}</p> ' + '</div>'
     + '{{#hasCTA}}<div class="o-feature-button clearfix"> '
     + '<a class=\"o-feature-action-button\" href="{{ctaUrl}}" target="_blank"><div>{{ctaText}}<\/div></a> '
     + '</div>{{/hasCTA}}</div> ' + '</div>'
     + '<div class="o-feature-clearfix"></div>';
 module.exports = templateCell;
-
 
